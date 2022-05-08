22f42369d84263efd37e9adcad84776475a888aa
KevinMBS <kevinoukv123@gmail.com>
2022-05-08 19:53:45 -0300

    Metodo post em /videos adicionado

--- src/controllers/videosController.js
+++ src/controllers/videosController.js
@@ -2,11 +2,22 @@ import videoModel from '../models/Video.js'
 
 class videosController{
     static listarVideos = (req, res) =>{
-        videoModel.find((err, Videos)=>{
-            res.status(200).json(Videos)
+        videoModel.find((err, videos)=>{
+            res.status(200).json(videos)
+        })  
+    }
+
+    static novoVideo = (req, res) =>{
+        let video = new videoModel(req.body)
+        video.save((err)=>{
+
+            if(err){
+                res.status(500).send({message: `${err.message} - Erro ao cadastrar novo  video`})
+            }else{
+                res.status(201).send(video.toJSON())
+            }
         })
-        
-    }   
+    }
 }
 
 export default videosController
--- src/routes/videosRoutes.js
+++ src/routes/videosRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router
     .get('/videos', videosController.listarVideos)
+    .post('/videos', videosController.novoVideo)
 
 export default router
